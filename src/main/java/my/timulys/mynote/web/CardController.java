466d2d6d32180740b0c5385ee0dd84f2952fa590
DESKTOP-035QAMS\jhchoi <timulys@gmail.com>
2018-08-16 18:51:05 +0900

    Tiles 적용 (진행중)

--- src/main/java/my/timulys/mynote/web/CardController.java
+++ src/main/java/my/timulys/mynote/web/CardController.java
@@ -1,22 +1,21 @@
 package my.timulys.mynote.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import my.timulys.mynote.service.CardService;
 
-@RestController
+@Controller
 @RequestMapping("/cards")
 public class CardController {
 	@Autowired
 	private CardService cardService;
 	
-	@GetMapping("")
+	@RequestMapping(value="/index")
 	public String home(Model model) {
 		model.addAttribute("cards", cardService.findAllCard());
-		return "index.html";
+		return "index";
 	}
 }
